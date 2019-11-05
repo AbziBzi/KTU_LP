b3c10a5023e069d0ff4a20b7d3638098dc421303
Edwin Zubowicz <39679085+AbziBzi@users.noreply.github.com>
2019-11-05 18:32:48 +0200

    Add files via upload

---
+++ LP_LAB2/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sync"
+)
+type Trees struct {
+	Trees []Tree `json:"trees"`
+}
+
+type Tree struct {
+	Type string `json:"type"`
+	Age int `json:"age"`
+	Height float64 `json:"height_m"`
+	Result int
+}
+
+var wg = sync.WaitGroup{}
+var FilterValue int = 92941
+var ThreadsCount int = 1
+var FilePath string = "data/IFF72_ZubowiczE_L1_dat_1.json"
+var ResultPath string = "data/IFF72_ZubowiczE_L1_rez.txt"
+
+func main() {
+	// Reads file and creates slice of tree struct
+	var trees = ReadJsonFile(FilePath)
+	var resultTrees []Tree
+
+	// Creates channels in which calculations will be done
+	worker := make(chan Tree)
+	receive := make(chan Tree)
+
+	// return all trees from chanel
+	for i := 0; i < ThreadsCount; i++{
+		go Execute(worker, receive)
+	}
+
+	// result printing function
+	go func(chanel <-chan Tree) {
+		for element := range chanel{
+			resultTrees = append(resultTrees, element)
+			wg.Done()
+		}
+	}(receive)
+
+	// Add trees to chanel
+	for _, tree := range trees{
+		wg.Add(1)
+		worker <- tree
+	}
+	wg.Wait()
+	WriteResultToFile(ResultPath, resultTrees, trees)
+}
+
+func Execute(chanel <-chan Tree, chanel2 chan<- Tree) {
+	for element := range chanel {
+		wg.Done()
+		tree := element
+		var value = FindPrimeNumber(tree)
+		if value <= FilterValue {
+			tree.Result = value
+			chanel2 <- tree
+			wg.Add(1)
+		}
+	}
+}
+
+// Calculations needed for filter trees
+func FindPrimeNumber(tree Tree) int{
+	var strLen = len(tree.Type)
+	var n = (int(tree.Height) * tree.Age * strLen)/2
+	var count int = 0
+	var a int = 2
+
+	for count < n {
+		var b int = 2
+		var prime int = 1
+		for b * b <= a {
+			if a % b == 0 {
+				prime = 0
+				break
+			}
+			b++
+		}
+		if prime > 0 {
+			count++
+		}
+		a++
+	}
+	return a-1
+}
+
+// Write results to result File
+func WriteResultToFile(resultPath string, resTrees []Tree, primTrees []Tree){
+	file, err := os.Create(resultPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = file.WriteString("Pradiniai duomenys:\n")
+	for _, tree := range primTrees{
+		s := fmt.Sprintf("| %-10s | %10d | %10.2f |", tree.Type, tree.Age, tree.Height)
+		_, err := fmt.Fprintln(file, s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	_, err = file.WriteString("\nSurušiuoti duomenys:\n")
+	for _, tree := range resTrees{
+		s := fmt.Sprintf("| %-10s | %10d | %10.2f | %10d |", tree.Type, tree.Age, tree.Height, tree.Result)
+		_, err := fmt.Fprintln(file, s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	err = file.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("file written successfully")
+}
+
+/// Reading from Json file, and adding data to array
+func ReadJsonFile(filePath string) []Tree {
+	file, _ := ioutil.ReadFile(filePath)
+	trees := Trees{}
+	_ = json.Unmarshal([]byte(file), &trees)
+	return trees.Trees
+}

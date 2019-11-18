f3b2f0b300ee5088c98c613cc8b8933623a640e4
Edwin Zubowicz <39679085+AbziBzi@users.noreply.github.com>
2019-11-19 00:52:36 +0200

    Add files via upload

--- LP_Projektas/main.go
+++ LP_Projektas/main.go
@@ -6,45 +6,132 @@ import (
 	"math/rand"
 )
 
-var a float64 = 5 // a is max cost of one length by two coordinates
+var a float64 = 2 // a is max cost of one length by two coordinates
 
-// MIN is minimum value of x or y
-var MIN = -10
+// Alpha is alpha
+var Alpha float64 = 0.01
 
-// MAX is maximum value of x or y
-var MAX = 10
+// Min is minimum value of x or y
+var Min = -10
 
-// COUNT is count of points
-var COUNT = 100
+// Max is maximum value of x or y
+var Max = 10
+
+// Count is count of points
+var Count = 100
+
+// MaxIterationCount is a condition to stop counting
+var MaxIterationCount = 100
 
 func main() {
 	fmt.Println("Start")
 
-	// var points = fillWithRandomPoints()
-	// fmt.Println(points)
+	var points = fillWithRandomPoints(Min, Max, Count)
+	findLowerCost(points, Alpha)
+}
 
-	var points = [][]int{{0, 0}, {1, 1}, {1, 2}, {2, 2}}
-	var cost = costFunction(points)
-	fmt.Println(cost)
+func findLowerCost(points [][]float64, alpha float64) {
+	var iterationsCount = 0
+	var result float64
+	fmt.Println(costFunction(points))
+
+	for iterationsCount < MaxIterationCount {
+		iterationsCount++
+		fValue := costFunction(points)
+		count := 0
+		gradientsVector := countGradient(points)
+		normalizeGradientVector(gradientsVector)
+		pointsCopy := makeCopy(points)
+		for i, point := range pointsCopy {
+			for j, xy := range point {
+				pointsCopy[i][j] = float64(xy) - (alpha * gradientsVector[count])
+				count++
+			}
+		}
+		fValueNext := costFunction(pointsCopy)
+		if fValueNext < fValue {
+			points = pointsCopy
+			result = fValueNext
+		} else {
+			alpha = alpha / 2
+		}
+	}
+	fmt.Println(result)
+}
+
+// Function that normalize vector of gradients
+func normalizeGradientVector(vector []float64) {
+	vectorsLength := countVectorsLength(vector)
+	for i, element := range vector {
+		vector[i] = element / vectorsLength
+	}
+}
+
+// Creates slice of counted points gradient
+func countGradient(points [][]float64) []float64 {
+	var gradients []float64
+	for i, point := range points {
+		for j := range point {
+			gradient := countPointGradient(i, j, 2, points)
+			gradients = append(gradients, gradient)
+		}
+	}
+	return gradients
+}
+
+// Counts point x or y gradient - where function grows faster
+func countPointGradient(xIndex, yIndex int, h float64, points [][]float64) float64 {
+	var pointsCopy = makeCopy(points)
+
+	pointsCopy[xIndex][yIndex] += h
+	var gradient = (costFunction(pointsCopy) - costFunction(points)) / float64(h)
+
+	return gradient
+}
+
+// Counts length of vector with n elements
+// Needed for normalize gradient vector
+func countVectorsLength(vector []float64) float64 {
+	var sum float64 = 0
+	for _, element := range vector {
+		sum += math.Pow(element, 2)
+	}
+	return math.Sqrt(sum)
+}
+
+// Clones 2d slice
+// Needed for counting point gradient
+func makeCopy(points [][]float64) [][]float64 {
+	pointsCopy := make([][]float64, len(points))
+
+	for i, point := range points {
+		pointCopy := make([]float64, len(point))
+		copy(pointCopy, point)
+		pointsCopy[i] = pointCopy
+
+	}
+	return pointsCopy
 }
 
 // C(l)= SUM((l-a)^2)
-func costFunction(points [][]int) float64 {
+// Counts cost of all lines between points
+func costFunction(points [][]float64) float64 {
 	var cost float64 = 0
+	counter := 0
 
 	for i, point := range points {
 		for _, pointNext := range points[i+1:] {
+			counter++
 			var length = countLength(point, pointNext)
 			var temp = length - a
-			cost = math.Pow(temp, 2)
+			cost += math.Pow(temp, 2)
 		}
 	}
-
 	return cost
 }
 
 // Counts length by two pints in 2d dimension
-func countLength(x1 []int, x2 []int) float64 {
+func countLength(x1 []float64, x2 []float64) float64 {
 	var xDif = x1[0] - x2[0]
 	var yDif = x1[1] - x2[1]
 	var xDifRaisedToTwo = math.Pow(float64(xDif), 2)
@@ -55,18 +142,20 @@ func countLength(x1 []int, x2 []int) float64 {
 }
 
 // Creates 2d slice with random points
-func fillWithRandomPoints() [][]int {
-	var primaryPoint = []int{0, 0}
-	points := make([][]int, COUNT)
+func fillWithRandomPoints(min, max, count int) [][]float64 {
+	var primaryPoint = []float64{0, 0}
+	points := make([][]float64, count)
 
 	for i := range points {
+		// rand.Seed(time.Now().UTC().UnixNano())
+		rand.Seed(1)
 		if i == 0 {
 			points[i] = primaryPoint
 			continue
 		}
-		var x = rand.Intn(MAX-MIN) + MIN
-		var y = rand.Intn(MAX-MIN) + MIN
-		var row = []int{x, y}
+		var x = rand.Intn(max-min) + min
+		var y = rand.Intn(max-min) + min
+		var row = []float64{float64(x), float64(y)}
 		points[i] = row
 
 	}
--- LP_Projektas/main_test.go
+++ LP_Projektas/main_test.go
@@ -5,43 +5,10 @@ import (
 	"testing"
 )
 
-func Test_main(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			main()
-		})
-	}
-}
-
-func Test_costFunction(t *testing.T) {
-	type args struct {
-		points [][]int
-	}
-	tests := []struct {
-		name string
-		args args
-		want float64
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := costFunction(tt.args.points); got != tt.want {
-				t.Errorf("costFunction() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func Test_countLength(t *testing.T) {
 	type args struct {
-		x1 []int
-		x2 []int
+		x1 []float64
+		x2 []float64
 	}
 	tests := []struct {
 		name string
@@ -50,14 +17,14 @@ func Test_countLength(t *testing.T) {
 	}{
 		{
 			args: args{
-				x1: []int{1, 1},
-				x2: []int{1, 1},
+				x1: []float64{1, 1},
+				x2: []float64{1, 1},
 			},
 			want: 0,
 		}, {
 			args: args{
-				x1: []int{0, 0},
-				x2: []int{0, 5},
+				x1: []float64{0, 0},
+				x2: []float64{0, 5},
 			},
 			want: 5,
 		},
@@ -71,17 +38,26 @@ func Test_countLength(t *testing.T) {
 	}
 }
 
-func Test_fillWithRandomPoints(t *testing.T) {
+func Test_clonePoints(t *testing.T) {
+	type args struct {
+		points [][]float64
+	}
 	tests := []struct {
 		name string
-		want [][]int
+		args args
+		want [][]float64
 	}{
-		// TODO: Add test cases.
+		{
+			args: args{
+				points: [][]float64{{0, 0}, {1, 1}, {-1, -1}},
+			},
+			want: [][]float64{{0, 0}, {1, 1}, {-1, -1}},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := fillWithRandomPoints(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("fillWithRandomPoints() = %v, want %v", got, tt.want)
+			if got := makeCopy(tt.args.points); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("clonePoints() = %v, want %v", got, tt.want)
 			}
 		})
 	}
